05be80a03893a98ca785122ab574b778d8020b2b
Francesco Cosentino <francesco.cosentino@ing.com>
2023-01-04 00:19:29 +0100

    added examples/list

---
+++ examples/list/list.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/hyp3rd/hypercache"
+	"github.com/hyp3rd/hypercache/types"
+)
+
+// This example demonstrates how to list items from the cache
+func main() {
+	// Create a new HyperCache with a capacity of 100
+	cache, err := hypercache.NewHyperCache(200,
+		hypercache.WithExpirationInterval(3*time.Second),
+		hypercache.WithEvictionInterval(3*time.Second))
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// Close the cache when the program exits
+	defer cache.Close()
+
+	// Add 100 items to the cache
+	for i := 0; i < 100; i++ {
+		key := fmt.Sprintf("key%d", i)
+		val := fmt.Sprintf("val%d", i)
+
+		err = cache.Set(key, val, time.Minute)
+
+		if err != nil {
+			fmt.Printf("unexpected error: %v\n", err)
+		}
+	}
+
+	// Retrieve the list of items from the cache
+	list, err := cache.List(
+		hypercache.WithSortBy(types.SortByValue),
+		hypercache.WithSortDescending(),
+		hypercache.WithFilter(func(item *hypercache.CacheItem) bool {
+			return item.Expiration > time.Second
+		}),
+	)
+
+	//
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Print the list of items
+	for i, ci := range list {
+		fmt.Println(i, ci.Value)
+	}
+}
