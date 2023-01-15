09e7005b0d04ce1280d9c24f44e117028c4412bd
Francesco Cosentino <francesco.cosentino@ing.com>
2023-01-15 10:25:50 +0100

    cache package moved to models

---
+++ stats/collector.go
@@ -0,0 +1,60 @@
+package stats
+
+import (
+	"fmt"
+
+	"github.com/hyp3rd/hypercache/errors"
+	"github.com/hyp3rd/hypercache/types"
+)
+
+// Collector is an interface that defines the methods that a stats collector should implement.
+type Collector interface {
+	// Incr increments the count of a statistic by the given value.
+	Incr(stat types.Stat, value int64)
+	// Decr decrements the count of a statistic by the given value.
+	Decr(stat types.Stat, value int64)
+	// Timing records the time it took for an event to occur.
+	Timing(stat types.Stat, value int64)
+	// Gauge records the current value of a statistic.
+	Gauge(stat types.Stat, value int64)
+	// Histogram records the statistical distribution of a set of values.
+	Histogram(stat types.Stat, value int64)
+	// GetStats returns the collected statistics.
+	GetStats() Stats
+}
+
+// StatsCollectorRegistry holds the a registry of stats collectors.
+var StatsCollectorRegistry = make(map[string]func() (Collector, error))
+
+// NewStatsCollector creates a new stats collector.
+// The statsCollectorName parameter is used to select the stats collector from the registry.
+func NewStatsCollector(statsCollectorName string) (Collector, error) {
+	// Check the parameters.
+	if statsCollectorName == "" {
+		return nil, fmt.Errorf("%s: %s", errors.ErrParamCannotBeEmpty, "statsCollectorName")
+	}
+
+	createFunc, ok := StatsCollectorRegistry[statsCollectorName]
+	if !ok {
+		return nil, fmt.Errorf("%s: %s", errors.ErrStatsCollectorNotFound, statsCollectorName)
+	}
+
+	return createFunc()
+}
+
+// RegisterStatsCollector registers a new stats collector with the given name.
+func RegisterStatsCollector(name string, createFunc func() (Collector, error)) {
+	StatsCollectorRegistry[name] = createFunc
+}
+
+func init() {
+	// Register the default stats collector.
+	RegisterStatsCollector("default", func() (Collector, error) {
+		var err error
+		collector := NewHistogramStatsCollector()
+		if collector == nil {
+			err = fmt.Errorf("%s: %s", errors.ErrStatsCollectorNotFound, "default")
+		}
+		return NewHistogramStatsCollector(), err
+	})
+}
